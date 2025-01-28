4e9d62183b25fa643d1ee84ec7319a42311a4d51
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-28 02:24:59 +0000

    palindrome number program using simple function

---
+++ leetcodePractice/palindromeNum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int palindrome(long long int n)
+{
+    long long temp = n;
+    int rev = 0;
+    while(temp)
+    {
+        rev = rev * 10 + (temp % 10);
+        temp /= 10;
+    }
+    if (rev == n)
+        return 1;
+    return 0;
+}
+int main()
+{
+    int n;
+    cout << "Enter number : ";
+    cin >> n;
+    int p = palindrome(n);
+    if(p)
+        cout << n << " is palindrome." << endl;
+    else 
+        cout << n << " is not a palidrome.\n";
+    cout << endl;
+}
