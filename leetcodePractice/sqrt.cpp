94787a924027f31e2885f39156c78aa4c359e2c3
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-01-28 02:49:09 +0000

    program for checking square root a number.

---
+++ leetcodePractice/sqrt.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter number : ";
+    cin >> n;
+    cout << "Square root of " << n << " is : " << sqrt(n) << endl;
+    return 0;
+}
