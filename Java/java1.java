07510df29fab242daf615564fbb171647ddc6195
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-02-12 18:57:34 +0000

    successfully runned

--- Java/java1.java
+++ Java/java1.java
@@ -1,7 +1,7 @@
 class Sample
 {
-    public static void main(Strings[] args)
+    public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World\n");
     }
 }
