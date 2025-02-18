33604841aceb3c96ce366a8d21f9eb2dbcf0df16
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-02-18 18:07:20 +0000

    Minimum Average of the largest and smallest element.

---
+++ leetcodePractice/minAvgofLargAndSmall.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        double minimumAverage(vector<int>& nums) {
+            sort(nums.begin(), nums.end());
+            double smallAvg = 214748364;
+            int i = 0;
+            int j = nums.size() - 1;
+            while(i < j)
+            {
+                if((double(nums[i] + nums[j])) / 2 < smallAvg)
+                    smallAvg = double(nums[i] + nums[j]) / 2;
+                i++;
+                j--;
+            }
+            return smallAvg;
+        }
+    };
+
+    int main()
+    {
+        Solution s;
+        vector<int>nums{7,8,3,4,15,13,4,1};
+        cout << "\nMinimum Average of the Smallest and Largest Element is : " << s.minimumAverage(nums);
+        return 0;
+    }
