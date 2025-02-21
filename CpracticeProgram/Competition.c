626125fb4da113897b1d17bd25b772484a854aa5
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-02-21 19:22:11 +0000

    basic program and malloc details in c language with their example

---
+++ CpracticeProgram/Competition.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//program 1: 
+// int main() {
+//     printf("%d %d %d", sizeof(3.14), sizeof(3.14f), sizeof(3.14L)); // double , float , double long 
+//     return 0;
+// }
+
+//program 2:
+// int main() {
+//     int x = 5;
+//     printf("%d\n", x++ + ++x);
+//     return 0;
+// }
+
+//program 3:
+// int main()
+// {
+//     int a = 10, *p;
+//     p = &a;
+//     printf("%d %d\n", *p, &p); // value of *p and its address which is own specific
+//     return 0;
+// }
+
+//program 4:
+
