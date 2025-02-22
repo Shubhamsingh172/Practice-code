8ad2f5ec1a4830a997c34943ee63733bc24cefe3
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-02-22 17:19:38 +0000

    question solved on loop which is printing the right angled traingle in c

--- CpracticeProgram/question.c
+++ CpracticeProgram/question.c
@@ -2,6 +2,15 @@
 
 int main()
 {
-    int n = 120;
-    printf("%d", 123456);
+    // int n = 120;
+    // printf("%d", 123456);
+    int n;
+    printf("Enter the number : ");
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+            printf("%d ", j);
+        printf("\n");
+    }
 }
