df5d292636332da0669be1fdac9a0ea021094d2c
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-02-28 18:41:55 +0000

    program till 7 questions

--- Clang/clang2.c
+++ Clang/clang2.c
@@ -30,14 +30,70 @@
 
 //program 4
 
-int main()
-{
-    int x = 1000; 
-    int y = 20000000;
+// int main()
+// {
+//     int x = 1000; 
+//     int y = 20000000;
 
-    //or 
-    int xx = 1000, yy = 2000; // result is always 1
-    int *p = &x, *q = &y;
-    printf("%d \n", q - p);
-    printf("%d \n", p - q);
+//     //or 
+//     int xx = 1000, yy = 2000; // result is always 1
+//     int *p = &x, *q = &y;
+//     printf("%d \n", q - p);
+//     printf("%d \n", p - q);
+//}
+
+
+//program 5 
+
+// #include <stdio.h>
+
+// int main() {
+//     int arr[] = {5, 10, 15, 20, 25};
+//     int *p = arr;
+    
+//     *(p++) += 10;
+//     *(++p) *= 2;
+
+//     printf("%d %d %d\n", arr[0], arr[1], arr[2]);
+//     return 0;
+// }
+
+
+//program 6 --> pointer and functions pointer 
+
+// #include <stdio.h>
+
+// void modify(int *ptr) {
+//     *(ptr++) += 5;
+//     *(++ptr) *= 3;
+// }
+
+// int main() {
+//     int arr[] = {2, 4, 6, 8, 10};
+//     modify(arr);
+    
+//     for(int i = 0; i < 5; i++) {
+//         printf("%d ", arr[i]);
+//     }
+//     return 0;
+// }
+
+//program 7 -->double pointers question 
+
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 10;
+    int *p = &a, *q = &b;
+    int **pp = &p, **qq = &q;
+
+    **pp += 2;
+    **qq -= 3;
+    *pp = q;
+    **pp *= 2;
+
+    printf("%d %d\n", a, b);
+    return 0;
 }
+
+//program 8
