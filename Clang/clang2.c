046cd4c1c7a56e791983130538a25636161b1f65
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-02-28 18:05:31 +0000

    c programming second question

--- Clang/clang2.c
+++ Clang/clang2.c
@@ -10,11 +10,12 @@
 //     return 0;
 // }
 
-//program 2
+//program 2 --> doubtful
 
-int main()
-{
-    int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int (*ptr)[3] = a + 2;
-    printf("%d\n%d\n%d\n",*(ptr + 0), *(ptr + 1), *(ptr + 2));
-}
+// int main()
+// {
+//     int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+//     int (*ptr)[3] = a + 1;
+//     printf("%d\n%d\n%d\n",*(ptr + 0)[0], *(ptr + 1)[0], *(ptr + 2)[0]);
+// }
+
