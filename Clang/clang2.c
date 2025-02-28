ee99ff574dfc03fd5516de6cf1c3a7867526b5f3
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-02-28 18:26:09 +0000

    c program 3 and 4

--- Clang/clang2.c
+++ Clang/clang2.c
@@ -19,3 +19,22 @@
 //     printf("%d\n%d\n%d\n",*(ptr + 0)[0], *(ptr + 1)[0], *(ptr + 2)[0]);
 // }
 
+//program 3 --> compiler dependent so we need to work upon this more for sure 
+
+// int main()
+// {
+//     int i = 2;
+//     printf("%d %d %d\n", ++i, i++, ++i);
+//     return 0;
+// }
+
+int main()
+{
+    int x = 1000; 
+    int y = 20000000;
+
+    //or 
+    int xx = 1000, yy = 2000; // result is always 1
+    int *p = &x, *q = &y;
+    printf("%d \n", q - p);
+}
