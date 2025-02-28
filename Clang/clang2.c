3483043aebba26d8087d9205a133e59fa03a119f
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-02-28 17:43:36 +0000

    programa 1 on pointers in c programming

---
+++ Clang/clang2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//program 1
+// int main()
+// {
+//     int arr[] = {10,20,30,40,50};
+//     int *ptr = arr;
+//     printf("%d\n", *(ptr + 2));
+//     printf("%d\n", *(ptr + 3));
+//     return 0;
+// }
+
+//program 2
+
+int main()
+{
+    int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int (*ptr)[3] = a + 2;
+    printf("%d\n%d\n%d\n",*(ptr + 0), *(ptr + 1), *(ptr + 2));
+}
