ec7887d63c4c2e58e1980fe339bc4939361a9ed3
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-03-01 03:33:27 +0000

    flag program in c programming language

---
+++ Clang/Cflags/flag2.c
@@ -0,0 +1,31 @@
+//program 1:
+
+// #include <stdio.h>
+// int main() 
+// {
+//     printf("|%+-8d|\n", 42);  
+//     printf("|% 08d|\n", 42);  
+//     printf("|%0+8d|\n", 42);
+//     return 0;
+// }
+
+
+//program 2:
+
+// #include <stdio.h>
+
+// int main() 
+// {
+//     printf("%-+10.2f\n", 123.456); // +123.46
+//     return 0;
+// }
+
+
+// program 3:
+#include <stdio.h>
+
+int main()
+{
+    printf("%.5s\n", "HelloWorld");
+    return 0;
+}
