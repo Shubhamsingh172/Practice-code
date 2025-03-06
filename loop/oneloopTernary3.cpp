e5bb1e055d6859c5bb48a361345a971f24aeee12
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-03-06 19:19:14 +0000

    just separate the code to line by line

--- loop/oneloopTernary3.cpp
+++ loop/oneloopTernary3.cpp
@@ -6,10 +6,7 @@ int main()
     int n;
     cout << "Enter the number : ";
     cin >> n;
-    for(int i = 1, spaces = 1; i <= n; (i <= n / 2 + 1) ? 
-    ((spaces <= ( n / 2 + 1) * 2 - (i * 2 - 1)) ? (cout << " ", spaces++) : 
-    (cout << "* ", spaces++)), ((spaces > (n / 2 + 1) * 2) ? 
-    (cout << endl, spaces = 1, i++): 0) : ((spaces <= i * 2 - n) ? 
-    (cout << " ", spaces++) : (cout << "* ", spaces++)), 
-    ((spaces > (n / 2 + 1) * 2) ? (cout << endl, spaces = 1, i++): 0));
+    for(int i = 1, spaces = 1; i <= n; (i <= n / 2 + 1) ? ((spaces <= ( n / 2 + 1) * 2 - (i * 2 - 1)) ? (cout << " ", spaces++) : 
+ (cout << "* ", spaces++)), ((spaces > (n / 2 + 1) * 2) ? (cout << endl, spaces = 1, i++): 0) : ((spaces <= i * 2 - n) ? 
+ (cout << " ", spaces++) : (cout << "* ", spaces++)), ((spaces > (n / 2 + 1) * 2) ? (cout << endl, spaces = 1, i++): 0));
 }
