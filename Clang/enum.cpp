ecfba266695327d1626f94a56fd7127e7426086c
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-03-14 07:15:08 +0000

    enumeration program in cpp

---
+++ Clang/enum.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+enum Day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+int main() {
+    // Write C++ code here
+    std::cout << "Try programiz.pro";
+    cout << endl << Monday << endl;
+    cout << Tuesday << endl;
+    cout << Wednesday << endl;
+    cout << Thursday << endl;
+    cout << Friday << endl;
+    cout << Saturday << endl;
+    cout << Sunday << endl;
+    return 0;
+}
