940c2000cbc1faf4625da97ae1d5bb6a1615b639
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-03-16 17:46:52 +0000

    singly linked list program in cpp simple program which considers insert pop and delete operation in it

---
+++ Linkelist/singlyPract1.cpp
@@ -0,0 +1,80 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+class Node 
+{
+    int data;
+    Node *next;
+    Node(int d)
+    {
+        data = d;
+        next = nullptr;
+    }
+    friend class linkedlist;
+};
+class linkedlist
+{
+    public:
+    Node *head;
+    linkedlist()
+    {
+        head = nullptr;
+    }
+    bool isfull()
+    {
+        return head == nullptr;
+    }
+    void insert(int data)
+    {
+        Node *temp = new Node(data);
+        if(temp == nullptr)
+        {
+            head = temp;
+            return;
+        }
+        else 
+        {
+            temp->next = head;
+            head = temp;
+        }
+    }
+    int Delete()
+    {
+        if(head == nullptr)
+            cout << "\nLinked list is empty....\n";
+        else 
+        {
+            Node *temp = head;
+            head = head->next;
+            if(head == nullptr)
+                head == nullptr;
+            return temp->data;
+        }
+    }
+    void display()
+    {
+        Node *temp = head;
+        cout << "Elements in the linkedlist are\n";
+        while(temp != nullptr)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+};
+int main() {
+    linkedlist ll;
+    ll.insert(10);
+    ll.insert(20);
+    ll.insert(30);
+    ll.insert(40);
+    ll.insert(50);
+    ll.insert(60);
+    ll.display();
+    cout << endl;
+    ll.Delete();
+    ll.display();
+    cout << endl;
+    return 0;
+}
