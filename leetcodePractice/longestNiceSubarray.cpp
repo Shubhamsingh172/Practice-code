141c0765b68c512d0d1f2a60c013133b9181042a
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-03-18 17:14:23 +0000

    added second or we can say third approach which is the optimal approach for this problem because first one was the brute force approach second one is big oh of n square and third one is sliding window approach.

--- leetcodePractice/longestNiceSubarray.cpp
+++ leetcodePractice/longestNiceSubarray.cpp
@@ -17,3 +17,23 @@
 //             return result;
 //         }
 //     };
+
+
+// solution using sliding window approach 
+//using sliding window approach 
+// int i = 0;
+// int j = 0;
+// int mask = 0;
+// int result = 1;
+// while(j < nums.size())
+// {
+//     while((mask & nums[j]) != 0) // keep shrinking the window 
+//     {
+//         mask ^= nums[i]; // or mask = (mask ^ nums[i]) like this 
+//         i++;
+//     }
+//     result = max(result, j - i + 1);
+//     mask |= nums[j]; // or mask = (mask | nums[j]);
+//     j++;
+// }
+// return result;
