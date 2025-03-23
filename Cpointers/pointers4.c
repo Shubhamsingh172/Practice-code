8b0aa6fb99605cd1bd69cc7db8f59b7fe33c8d6f
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-03-23 02:16:15 +0000

    using dynamic array allocation we store the address of row in array of pointers means we declare an array where we store the address of each row of element.

---
+++ Cpointers/pointers4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int matrix[][4] = {
+                        {1,2,3,4},
+                        {4,5,6,7},
+                        {7,8,9,0}
+                      };
+    int *arr[3];
+
+    arr[0] = &matrix[0][0];
+    arr[1] = &matrix[1][0];
+    arr[2] = &matrix[2][0];
+
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < sizeof(matrix[i])/sizeof(int); j++)
+            printf("%2d", arr[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
