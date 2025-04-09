fd82841b6369c6452fec6f7a2801e950576f904c
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-04-09 18:17:55 +0000

    coe is working on other compilers

---
+++ C++/palindrome.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string str = "naman";
+    string rev = str;
+    reverse(rev.begin(), rev.end());
+    if(rev == str)
+        cout << "Palindrome" << endl;
+    else 
+        cout << "Not a palindrome." << endl;
+}
