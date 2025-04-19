da8f7413389b06fae19db01b78e87cdc0cd1a77e
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-04-19 03:11:20 +0000

    in 2 loop

---
+++ loop/ignitraPattern.cpp
@@ -0,0 +1,31 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 4; i++)
+    {
+        int temp = 4;
+        int val = i;
+        for(int j = 1; j <= i; j++)
+        {
+            if(i == j * i) cout << val << " ";
+            else 
+            {
+                val += temp;
+                cout << val << " ";
+                temp--;
+            }
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
+
+/*
+1
+2 6
+3 7 10
+4 8 11 13
+*/
